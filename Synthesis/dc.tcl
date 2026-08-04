f1f15d20d2bfacb9a3ef81c19f39b105e3d32534
Hitanshu22 <hitanshuparikh05@gmail.com>
2026-08-04 19:52:20 +0530

    Update dc.tcl

--- Synthesis/dc.tcl
+++ Synthesis/dc.tcl
@@ -1,1 +1,64 @@
+##################################################
+# Setup
+##################################################
+
+set project_path /home/student/prac
+set_app_var search_path [list . $project_path/Design \
+                       /home/student/prac/Workshop/ref/lib/ndm
+]
+set_app_var target_library "/home/student/prac/Workshop/ref/lib/stdcell_rvt/saed32rvt_ss0p7vn40c.db"
+set_app_var link_library "* /home/student/prac/Workshop/ref/lib/stdcell_rvt/saed32rvt_ss0p7vn40c.db"
+
+##################################################
+# Read RTL
+##################################################
+
+analyze -format verilog /home/student/prac/Design/fifo.v
+elaborate fifo
+
+current_design fifo
+link
+
+##################################################
+# Read Constraints
+##################################################
+
+source /home/student/prac/Design/fifo.sdc
+
+##################################################
+# Design Checks
+##################################################
+
+check_design
+check_timing
+
+##################################################
+# Compile
+##################################################
+
+compile_ultra
+
+##################################################
+# Reports
+##################################################
+
+report_area > /home/student/prac/Report/report_area.rpt
+report_power > /home/student/prac/Report/report_power.rpt
+report_timing > /home/student/prac/Report/report_timing.rpt
+report_qor > /home/student/prac/Report/report_qor.rpt
+report_constraints > /home/student/prac/Report/report_constraints.rpt
+
+##################################################
+# Output Files
+##################################################
+
+write_file -format verilog -output /home/student/prac/Output/fifo_mapped.vg
+
+write_sdc /home/student/prac/Output/fifo_mapped.sdc
+
+##################################################
+# Exit
+##################################################
+
+#exit
 
